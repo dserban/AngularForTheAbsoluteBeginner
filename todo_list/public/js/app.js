d4cb489a3f44ebefc88299c668d2d9c2c16d68e5
Dan Serban <dserban01@yahoo.com>
2015-11-04 19:26:55 +0200

    With AJAX template

--- todo_list/public/js/app.js
+++ todo_list/public/js/app.js
@@ -15,3 +15,12 @@ app.controller('AngularToDoListController', ['$scope', '$http', function($scope,
 
 }]);
 
+/*
+    $http.post('/endpoint', { 'k1': $scope.v1, 'k2': $scope.v2 })
+      .success(function(data, status, headers, config) {
+        // code goes here
+      })
+      .error(function(data, status, headers, config) {
+        console.log('Error when requesting something-something.');
+      });
+*/
